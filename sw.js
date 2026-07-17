f0bedbed1b648bf0fd66cf2bf892897bc027b093
A <peter@vokrot.com>
2026-07-17 12:38:49 -0400

    gui fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Plate Scanner service worker: offline-first for models/runtime, network-first for pages.
-const CACHE = 'alpr-v7';
+const CACHE = 'alpr-v11';
 const SHELL = ['./index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
