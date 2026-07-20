c5e234c2d9acf66a9b01e07c9e2190533520db2a
A <peter@vokrot.com>
2026-07-20 12:00:57 -0400

    3.2 multithread try

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Plate Scanner service worker: offline-first for models/runtime, network-first for pages.
-const CACHE = 'alpr-v13';
+const CACHE = 'alpr-v14';
 const SHELL = ['./index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png',
                './geocode-us.json'];
 
